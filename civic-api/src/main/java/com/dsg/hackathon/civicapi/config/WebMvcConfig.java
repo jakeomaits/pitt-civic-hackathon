942e7d6c73874d67cb3485d26088a57e1d0dd3c5
Ty Garside <tyrone.garside@dcsg.com>
2021-01-27 18:00:32 -0500

    adding heroku deployment

---
+++ civic-api/src/main/java/com/dsg/hackathon/civicapi/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.dsg.hackathon.civicapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "index.html");
+    }
+
+}
