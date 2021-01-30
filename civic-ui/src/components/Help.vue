3f4cf399c4dc97efc3f706557ea88f8ef0ceb1ab
Austin Reed <Austin.Reed@dcsg.com>
2021-01-30 14:09:16 -0500

    added Help component

---
+++ civic-ui/src/components/Help.vue
@@ -0,0 +1,26 @@
+
+<template>
+<div>
+  <b-card
+    :title=help.title
+    tag="helpRequest"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      {{help.description}}
+    </b-card-text>
+    <div>
+      {{help.location}}
+    </div>
+
+    <b-button href="#" variant="primary">Accept Request</b-button>
+  </b-card>
+</div>
+</template>
+
+<script>
+    export default {
+      props: ['help']
+    }
+</script>
