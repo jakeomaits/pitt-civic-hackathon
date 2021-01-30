f5ff4069d68ea6c4f1db42c826cf2a212f0dc652
Austin Reed <Austin.Reed@dcsg.com>
2021-01-30 10:11:05 -0500

    added NavBar

--- civic-ui/src/App.vue
+++ civic-ui/src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+
+</script>
+
 
 <style lang="scss">
 #app {
---
+++ civic-ui/src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Good Sammy</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/">Feed</b-nav-item>
+          <b-nav-item href="#">Profile</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
