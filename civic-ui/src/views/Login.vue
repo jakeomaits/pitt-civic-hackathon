766026d02867c441ab05257879e39aa33e05cb08
Jacob Omaits <jacob.omaits@dcsg.com>
2021-01-31 11:43:54 -0500

    login styling updated

--- civic-ui/src/views/Login.vue
+++ civic-ui/src/views/Login.vue
@@ -1,39 +1,39 @@
 <template>
-  <b-container fluid>
-    <b-card bg-variant="light" class="mb-2">
+  <div>
+    <h1 style="margin-top:100px;margin-bottom:30px;">Olwaz Placeholder</h1>
+  <b-container fluid >
       <b-form @submit="onSubmit">
         <b-form-group
           label-cols-lg="3"
-          label="Login"
+          label="Welcome Back!"
           label-size="lg"
           label-class="font-weight-bold pt-0"
           class="mb-0"
         >
-
           <b-form-group
             label="Email:"
             label-for="login-email"
             label-cols-sm="3"
-            label-align-sm="right"
+            label-align="left"
           >
-            <b-form-input id="login-email" type="email" ></b-form-input>
+            <b-form-input id="login-email" type="email" placeholder="ex. example@email.com" ></b-form-input>
           </b-form-group>
 
           <b-form-group
               label="Password:"
               label-for="login-password"
               label-cols-sm="3"
-              label-align-sm="right"
+              label-align="left"
               type="password"
           >
             <b-form-input id="login-password" type="password" ></b-form-input>
           </b-form-group>
 
         </b-form-group>
-        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button squared type="submit" variant="primary" class="main-button">Sign In</b-button>
       </b-form>
-    </b-card>
   </b-container>
+  </div>
 </template>
 
 <script>
@@ -70,5 +70,13 @@ li {
 }
 a {
   color: #42b983;
+}
+.main-button {
+  height: 42px;
+  width: 189px;
+  background: #007DA3;
+  margin-top: 30px;
+  font-weight: bold;
+  width: 100%;
 }
 </style>
